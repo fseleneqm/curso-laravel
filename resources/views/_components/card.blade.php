92c471c1f33ba9dca9bc122ed62e2c42f3e611e1
Selene <fseleneqm@gmail.com>
2025-09-26 20:23:17 +0100

    margin mayor

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,4 +1,4 @@
-<div style="border: 1px solid black; border-radius: 8px; padding: 10px; margin: 5px;">
+<div style="border: 1px solid black; border-radius: 8px; padding: 10px; margin: 10px;">
     <h3>{{ $title }}</h3>
     <p>{{ $content }}</p>
 </div>
