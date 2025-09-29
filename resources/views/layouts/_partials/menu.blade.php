61b8fa1a073d9b79ada99e82adb808eb628f9196
Selene <fseleneqm@gmail.com>
2025-09-29 18:57:22 +0100

    agregamos etiqueta header

--- resources/views/layouts/_partials/menu.blade.php
+++ resources/views/layouts/_partials/menu.blade.php
@@ -1,8 +1,10 @@
-<nav>
-    <ul> <!-- ul>li*4>a -->
-        <li><a href="{{route('index')}}">Home</a></li>
-        <li><a href="{{route('about')}}">About</a></li>
-        <li><a href="{{route('services')}}">Services</a></li>
-        <li><a href="{{route('contact')}}">Contact</a></li>
-    </ul>
-</nav>
+<header>
+    <nav>
+        <ul> <!-- ul>li*4>a -->
+            <li><a href="{{route('index')}}">Home</a></li>
+            <li><a href="{{route('about')}}">About</a></li>
+            <li><a href="{{route('services')}}">Services</a></li>
+            <li><a href="{{route('contact')}}">Contact</a></li>
+        </ul>
+    </nav>
+</header>
