6ad5710c5d82baa807b391854f59faeae62583a3
Selene <fseleneqm@gmail.com>
2025-09-29 19:18:17 +0100

    A pesar de cambiar la ruta de la página, si añadimos asset a la ruta de la img, no hay problema

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/','index')->name('index');
 Route::view('/about','about')->name('about');
-Route::view('/services','services')->name('services');
+Route::view('/services/example','services')->name('services');
 Route::view('/contact','contact')->name('contact');
