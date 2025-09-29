44648c4e3cd60a5885eb1971d5d9d504a728fffe
Selene <fseleneqm@gmail.com>
2025-09-29 19:56:46 +0100

    estilos de los card

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,4 +1,4 @@
-<div style="border: 1px solid black; border-radius: 8px; padding: 10px; margin: 10px;">
+<div class="card" style="border: 1px solid black; border-radius: 8px; padding: 10px; margin: 10px;">
     <h3>{{ $title }}</h3>
     <img src="{{ asset('assets/img/lago.jpg') }}" alt="Imagen de un lago entre montañas" width=150>
     <p>{{ $content }}</p>
