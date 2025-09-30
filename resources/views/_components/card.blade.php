77bc7ef9c720d09cad10c44b749fbdb4ed693816
Selene <fseleneqm@gmail.com>
2025-09-30 16:52:12 +0100

    correcciones

--- resources/views/_components/card.blade.php
+++ resources/views/_components/card.blade.php
@@ -1,7 +1,6 @@
-<div class="cards">
-    <div class="card" style="border: 1px solid black; border-radius: 8px; padding: 10px; margin: 10px;">
-        <h3>{{ $title }}</h3>
-        <img src="{{ asset('assets/img/lago.jpg') }}" alt="Imagen de un lago entre montañas" width=150>
-        <p>{{ $content }}</p>
-    </div>
+<div class="card">
+    <h3>{{ $title }}</h3>
+    <img src="{{ asset('assets/img/lago.jpg') }}" alt="Imagen de un lago entre montañas" width=150>
+    <p>{{ $content }}</p>
 </div>
+
